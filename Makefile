14966f9f97a3b70a85a1ce44998fb53128ed8832
mulhern <amulhern@redhat.com>
2016-07-19 16:09:13 -0400

    Initial framework.
    Signed-off-by: mulhern <amulhern@redhat.com>

---
+++ Makefile
@@ -0,0 +1,34 @@
+TOX=tox
+
+.PHONY: lint
+lint:
+	$(TOX) -c tox.ini -e lint
+
+.PHONY: coverage
+coverage:
+	$(TOX) -c tox.ini -e coverage
+
+.PHONY: test
+test:
+	$(TOX) -c tox.ini -e test
+
+PYREVERSE_OPTS = --output=pdf
+.PHONY: view
+view:
+	-rm -Rf _pyreverse
+	mkdir _pyreverse
+	PYTHONPATH=src pyreverse ${PYREVERSE_OPTS} --project="cli" src/cli
+	mv classes_cli.pdf _pyreverse
+	mv packages_cli.pdf _pyreverse
+
+.PHONY: archive
+archive:
+	git archive --output=./cli.tar.gz HEAD
+
+.PHONY: upload-release
+upload-release:
+	python setup.py register sdist upload
+
+.PHONY: docs
+docs:
+	cd doc/_build/html; zip -r ../../../docs *
---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import setuptools
+if sys.version_info[0] < 3:
+    from codecs import open
+
+def local_file(name):
+    return os.path.relpath(os.path.join(os.path.dirname(__file__), name))
+
+README = local_file("README.rst")
+
+with open(local_file("src/cli/_version.py")) as o:
+        exec(o.read())
+
+setuptools.setup(
+    name='cli',
+    version=__version__,
+    author='Anne Mulhern',
+    author_email='mulhern@cs.wisc.edu',
+    description='conversion of ints and rationals to any base',
+    long_description=open(README, encoding='utf-8').read(),
+    platforms=['Linux'],
+    license='GPL 2+',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        ],
+    install_requires = [
+        'six'
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages("src"),
+    )
