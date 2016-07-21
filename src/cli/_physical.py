20a2d006459a05b694ae755f29c5e28ec5a0423b
mulhern <amulhern@redhat.com>
2016-07-21 10:49:35 -0400

    Factor out physical into a separate directory.
    Signed-off-by: mulhern <amulhern@redhat.com>

---
+++ src/cli/_physical.py
@@ -0,0 +1,95 @@
+import argparse
+
+
+def build_physical_add_parser(parser):
+    """
+    Generates the parser appropriate for adding devices to a pool.
+
+    :param ArgumentParser parser: a parser
+    :returns: a completed parser for adding devices
+    :rtype: ArgumentParser
+    """
+    parser.add_argument(
+       'name',
+       action='store',
+       help='pool name'
+    )
+    parser.add_argument(
+       'device',
+       help='make device D a member of this pool',
+       metavar='D',
+       nargs='+'
+    )
+    return parser
+
+
+def build_physical_list_parser(parser):
+    """
+    Generates the parser appropriate for listing information about physical
+    aspects of a pool.
+
+    :param ArgumentParser parser: a parser
+    :returns: a completed parser for listing physical information about a pool
+    :rtype: ArgumentParser
+    """
+    return parser
+
+
+def build_physical_remove_parser(parser):
+    """
+    Generates the parser appropriate for removing physical devices from a pool.
+
+    :param ArgumentParser parser: a parser
+    :returns: a completed parser for removing physical devices from a pool
+    :rtype: ArgumentParser
+    """
+    parser.add_argument(
+       'name',
+       action='store',
+       help='pool name'
+    )
+    parser.add_argument(
+       'device',
+       help='remove device D from this pool',
+       metavar='D',
+       nargs='+'
+    )
+    return parser
+
+
+def build_physical_parser(parser):
+    """
+    Generates the parser appropriate for administering physical aspects of
+    a specified pool.
+
+    :param ArgumentParser parser: a parser
+    :returns: a completed parser for administering physical aspects of a pool
+    :rtype: ArgumentParser
+    """
+
+    subparsers = parser.add_subparsers(title='subcommands')
+
+    subparser_table = dict()
+
+    subparser_table['add'] = \
+       subparsers.add_parser(
+          'add',
+          description="Add Devices to a Pool"
+       )
+    build_physical_add_parser(subparser_table['add'])
+
+    subparser_table['list'] = \
+       subparsers.add_parser(
+          'list',
+          description="List Pool Information"
+       )
+    build_physical_list_parser(subparser_table['list'])
+
+    subparser_table['remove'] = \
+       subparsers.add_parser(
+          'remove',
+          description="Remove Devices from a Pool"
+       )
+    build_physical_remove_parser(subparser_table['remove'])
+
+    return parser
