1c14dcedc8e025d0bd154d6dd14cd3ac7eb1d15f
mulhern <amulhern@redhat.com>
2016-08-03 16:25:50 -0400

    Start setting up the dbus.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+import dbus
+
 from cli import gen_parser
 
 def main():
     parser = gen_parser()
     args = parser.parse_args()
     print(args)
+    session_bus = dbus.SessionBus()
 
 if __name__ == "__main__":
     main()
