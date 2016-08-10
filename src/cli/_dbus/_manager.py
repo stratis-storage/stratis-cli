f74c60c571142d3242c63d62c8409ff1faed57d5
mulhern <amulhern@redhat.com>
2016-08-10 13:20:34 -0400

    Add two new methods to the Manager interface.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/cli/_dbus/_manager.py
+++ src/cli/_dbus/_manager.py
@@ -46,6 +46,30 @@ class Manager(object):
            dbus_interface=self._INTERFACE_NAME
         )
 
+    def GetPoolObjectPath(self, pool_name):
+        """
+        Get the object path of a pool.
+
+        :param str pool_name: the name of the pool
+        """
+        return self._dbus_object.GetPoolObjectPath(
+           pool_name,
+           dbus_interface=self._INTERFACE_NAME
+        )
+
+    def GetVolumeObjectPath(self, pool_name, volume_name):
+        """
+        Get the object path of volume ``volume_name`` in pool ``pool_name``.
+
+        :param str pool_name: the pool name
+        :param str volume_name: the volume name
+        """
+        return self._dbus_object.GetVolumeObjectPath(
+           pool_name,
+           volume_name,
+           dbus_interface=self._INTERFACE_NAME
+        )
+
     def ListPools(self):
         """
         List all pools.
