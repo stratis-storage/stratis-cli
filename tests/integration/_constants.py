a100c5568391ba1c4ce5c4d17d53ea309be60776
mulhern <amulhern@redhat.com>
2016-08-15 09:59:38 -0400

    Factor out some constants into separate file.
    Signed-off-by: mulhern <amulhern@redhat.com>

---
+++ tests/integration/_constants.py
@@ -0,0 +1,7 @@
+import pyudev
+
+_STRATISD = '/home/mulhern/my-contributions/stratisd'
+_STRATISD_EXECUTABLE = 'stratisd'
+_CLI = '/home/mulhern/my-projects/cli/src/main.py'
+
+_DEVICES = list(pyudev.Context().list_devices(subsystem='block'))
