fdc6b04934ae5ddca395e2261f445aff0bd91a02
mulhern <amulhern@redhat.com>
2016-08-16 12:09:29 -0400

    Add a skipped test about volumes in pool.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/integration/test_destroy.py
+++ tests/integration/test_destroy.py
@@ -129,3 +129,73 @@ class Destroy2TestCase(unittest.TestCase):
             subprocess.check_call(command_line)
         except subprocess.CalledProcessError:
             self.fail("Should not fail because pool is not there.")
+
+
+@unittest.skip('No way to determine status of volumes in pool.')
+class Destroy3TestCase(unittest.TestCase):
+    """
+    Test 'destroy' on database which contains the given pool and a volume.
+
+    Verify that the volume is gone when the pool is gone.
+    """
+    _MENU = ['destroy']
+    _POOLNAME = 'deadpool'
+    _VOLNAME = 'vol'
+
+    def setUp(self):
+        """
+        Start the stratisd daemon with the simulator.
+        """
+        env = dict(os.environ)
+        env['LD_LIBRARY_PATH'] = os.path.join(_STRATISD, 'lib')
+
+        bin_path = os.path.join(_STRATISD, 'bin')
+
+        self._stratisd = subprocess.Popen(
+           os.path.join(bin_path, _STRATISD_EXECUTABLE),
+           env=env
+        )
+        command_line = \
+           ['python', _CLI, 'create'] + \
+           [self._POOLNAME] + \
+           [d.device_node for d in _device_list(_DEVICES, 1)]
+        subprocess.check_call(command_line)
+
+        command_line = \
+           ['python', _CLI, 'logical', 'create'] + \
+           [self._POOLNAME] + \
+           [self._VOLNAME]
+        subprocess.check_call(command_line)
+
+    def tearDown(self):
+        """
+        Stop the stratisd simulator and daemon.
+        """
+        self._stratisd.terminate()
+
+    def testWithoutForce(self):
+        """
+        Whether or not destroy succeeds depends on the state of the pool.
+        """
+        try:
+            command_line = \
+               ['python', _CLI] + \
+               self._MENU + \
+               [self._POOLNAME]
+            subprocess.check_call(command_line)
+        except subprocess.CalledProcessError:
+            self.fail("Should not fail because pool is not there.")
+
+    def testWithForce(self):
+        """
+        Whether or not destroy succeeds depends on the state of the pool.
+        """
+        try:
+            command_line = \
+               ['python', _CLI] + \
+               self._MENU + \
+               ['--force'] + \
+               [self._POOLNAME]
+            subprocess.check_call(command_line)
+        except subprocess.CalledProcessError:
+            self.fail("Should not fail because pool is not there.")
