3490e2f82bfc7ecca22b7d19ffefb95837c4e36e
mulhern <amulhern@redhat.com>
2016-08-19 17:09:47 -0400

    Test listing devices in pool.
    Signed-off-by: mulhern <amulhern@redhat.com>

---
+++ tests/integration/physical/test_list.py
@@ -0,0 +1,77 @@
+"""
+Test 'create'.
+"""
+
+import unittest
+
+from cli._main import run
+from cli._errors import StratisCliRuntimeError
+from cli._stratisd_constants import StratisdErrorsGen
+
+from .._constants import _DEVICES
+
+from .._misc import _device_list
+from .._misc import Service
+
+
+class ListTestCase(unittest.TestCase):
+    """
+    Test listing devices for a non-existant pool.
+    """
+    _MENU = ['physical', 'list']
+    _POOLNAME = 'deadpool'
+
+    def setUp(self):
+        """
+        Start the stratisd daemon with the simulator.
+        """
+        self._service = Service()
+        self._service.setUp()
+
+    def tearDown(self):
+        """
+        Stop the stratisd simulator and daemon.
+        """
+        self._service.tearDown()
+
+    def testList(self):
+        """
+        Listing the devices must fail since the pool does not exist.
+        """
+        command_line = self._MENU + [self._POOLNAME]
+        with self.assertRaises(StratisCliRuntimeError) as cm:
+            all(run(command_line))
+        expected_error = StratisdErrorsGen.get_object().STRATIS_POOL_NOTFOUND
+        self.assertEqual(cm.exception.rc, expected_error)
+
+
+class List2TestCase(unittest.TestCase):
+    """
+    Test listing devices in an existing pool.
+    """
+    _MENU = ['physical', 'list']
+    _POOLNAME = 'deadpool'
+
+    def setUp(self):
+        """
+        Start the stratisd daemon with the simulator.
+        """
+        self._service = Service()
+        self._service.setUp()
+        command_line = \
+           ['create'] + [self._POOLNAME] + \
+           [d.device_node for d in _device_list(_DEVICES, 1)]
+        all(run(command_line))
+
+    def tearDown(self):
+        """
+        Stop the stratisd simulator and daemon.
+        """
+        self._service.tearDown()
+
+    def testList(self):
+        """
+        Listing the devices should succeed.
+        """
+        command_line = self._MENU + [self._POOLNAME]
+        all(run(command_line))
