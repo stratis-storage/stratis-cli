219dae8cf22f7fc6d240e56944636ae55063fb36
mulhern <amulhern@redhat.com>
2016-08-26 15:08:12 -0400

    Restrict devices to only disks (not partition).
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/integration/_constants.py
+++ tests/integration/_constants.py
@@ -6,4 +6,5 @@ import pyudev
 _STRATISD = '/home/mulhern/my-contributions/stratisd'
 _STRATISD_EXECUTABLE = 'stratisd'
 
-_DEVICES = list(pyudev.Context().list_devices(subsystem='block'))
+_DEVICES = \
+   [x for x in pyudev.Context().list_devices(subsystem='block', DEVTYPE='disk')]
