646b606588601cf8398bdb009f3a94cc2cfd4398
mulhern <amulhern@redhat.com>
2016-10-18 08:52:07 -0400

    Add additional tests that verify the value and the generated signature.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/dbus/test_signature_parser.py
+++ tests/dbus/test_signature_parser.py
@@ -206,6 +206,95 @@ def _descending(dbus_object):
         return variant_level if variant_level in (0, 1) else None
 
 
+def _extract_signature(dbus_object):
+    """
+    Get the signature of a dbus object.
+
+    :param dbus_object: the object
+    :type dbus_object: a dbus object
+    :returns: the corresponding signature
+    :rtype: str
+    """
+    # pylint: disable=too-many-return-statements
+    # pylint: disable=too-many-branches
+
+    if dbus_object.variant_level != 0:
+        return 'v'
+
+    if isinstance(dbus_object, dbus.Array):
+        sigs = frozenset(_extract_signature(x) for x in dbus_object)
+        len_sigs = len(sigs)
+        if len_sigs > 1:
+            raise ValueError("bad object")
+
+        if len_sigs == 0:
+            return 'a' + dbus_object.signature
+
+        return 'a' + [x for x in sigs][0]
+
+    if isinstance(dbus_object, dbus.Struct):
+        sigs = (_extract_signature(x) for x in dbus_object)
+        return '(' + "".join(x for x in sigs) + ')'
+
+    if isinstance(dbus_object, dbus.Dictionary):
+        key_sigs = frozenset(_extract_signature(x) for x in dbus_object.keys())
+        value_sigs = \
+           frozenset(_extract_signature(x) for x in dbus_object.values())
+
+        len_key_sigs = len(key_sigs)
+        len_value_sigs = len(value_sigs)
+
+        if len_key_sigs != len_value_sigs:
+            raise ValueError("bad object")
+
+        if len_key_sigs > 1:
+            raise ValueError("bad object")
+
+        if len_key_sigs == 0:
+            return 'a{' + dbus_object.signature + '}'
+
+        return 'a{' + [x for x in key_sigs][0] + [x for x in value_sigs][0] + '}'
+
+    if isinstance(dbus_object, dbus.Boolean):
+        return 'b'
+
+    elif isinstance(dbus_object, dbus.Byte):
+        return 'y'
+
+    elif isinstance(dbus_object, dbus.Double):
+        return 'd'
+
+    elif isinstance(dbus_object, dbus.Int16):
+        return 'n'
+
+    elif isinstance(dbus_object, dbus.Int32):
+        return 'i'
+
+    elif isinstance(dbus_object, dbus.Int64):
+        return 'x'
+
+    elif isinstance(dbus_object, dbus.ObjectPath):
+        return 'o'
+
+    elif isinstance(dbus_object, dbus.Signature):
+        return 'g'
+
+    elif isinstance(dbus_object, dbus.String):
+        return 's'
+
+    elif isinstance(dbus_object, dbus.UInt16):
+        return 'q'
+
+    elif isinstance(dbus_object, dbus.UInt32):
+        return 'u'
+
+    elif isinstance(dbus_object, dbus.UInt64):
+        return 't'
+
+    elif isinstance(dbus_object, dbus.types.UnixFd):
+        return 'h'
+
+
 class ParseTestCase(unittest.TestCase):
     """
     Test parsing various signatures.
@@ -218,6 +307,13 @@ class ParseTestCase(unittest.TestCase):
     def testParsing(self, signature):
         """
         Test that parsing is always succesful.
+
+        Verify that the original signature corresponds to the signature
+        returned by the parser and to the signature of the generated value.
+
+        Verify that the variant levels always descend within the constructed
+        value, always by single steps and that leaves of the value always
+        have variant level of 0 or 1.
         """
         base_type_objects = [
            x.example() for x in \
@@ -231,7 +327,10 @@ class ParseTestCase(unittest.TestCase):
         values = [v for (v, _) in results]
         levels = [l for (_, l) in results]
 
-        for sig, (level, value) in zip(sigs, zip(levels, values)):
-            if 'v' not in sig:
+        for sig_orig, (sig_synth, (level, value)) in \
+           zip(dbus.Signature(signature), zip(sigs, zip(levels, values))):
+            self.assertEqual(sig_orig, sig_synth)
+            if 'v' not in sig_orig:
                 self.assertEqual(level, 0)
-            self.assertTrue(_descending(value) != None)
+            self.assertIsNotNone(_descending(value))
+            self.assertEqual(_extract_signature(value), sig_orig)
