3532bac28c10b25f543057d59617691ae9875308
mulhern <amulhern@redhat.com>
2017-11-01 19:37:06 -0400

    Delete some obsoleted constants
    SERVICE_UNKNOWN_ERROR is unused and can be redefined if necessary.
    REDUNDANCY is now handled differently, in a different file.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/stratis_cli/_actions/_constants.py
+++ src/stratis_cli/_actions/_constants.py
@@ -20,10 +20,4 @@ SERVICE = 'org.storage.stratis1'
 TOP_OBJECT = '/org/storage/stratis1'
 MANAGER_INTERFACE = 'org.storage.stratis1.Manager'
 
-SERVICE_UNKNOWN_ERROR = 'org.freedesktop.DBus.Error.ServiceUnknown'
-
-REDUNDANCY = {
-   'none' : 'STRATIS_RAID_TYPE_SINGLE'
-}
-
 SECTOR_SIZE = 512
