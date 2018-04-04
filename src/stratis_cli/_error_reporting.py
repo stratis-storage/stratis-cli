80d3c79b42ed696a9c5025016ba7b6620d39211b
mulhern <amulhern@redhat.com>
2018-04-04 13:43:01 -0400

    Print a list of error messages instead of just the top one
    If there is no printable string for any error message raise exception.
    This will encourage users to report what should never happen.
    
    Print the error messages out in a conversational way.
    Start with the lowest level and move up to the highest level.
    Interpolate "which in turn caused" in between error messages.
    
    Include __context__ as well as __cause__ in chain of causes.
    __context__ field is set if there is no "from" after the exception.
    But that is just as likely to mean that the developer of the library
    did not bother to use "from" than that from does not apply. In other words,
    this seems like a backwards compatibility effort, more than anything, so
    treat __context__ and __cause__ the same when reporting errors.
    
    Put error reporting into a separate module. It is still quite limited,
    but the plan is to make it more interesting, at least a module's worth.
    
    Resolves: #83
    
    Signed-off-by: mulhern <amulhern@redhat.com>

---
+++ src/stratis_cli/_error_reporting.py
@@ -0,0 +1,67 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Facilities for managing and reporting errors.
+"""
+import os
+import sys
+
+import dbus
+
+
+def get_errors(exc):
+    """
+    Generates a sequence of exceptions starting with exc and following the chain
+    of causes.
+    """
+    while True:
+        yield exc
+        exc = getattr(exc, "__cause__") or getattr(exc, "__context__")
+        if exc is None:
+            return
+
+
+def get_error_msgs(errors):
+    """
+    Generates messages from a sequence of errors. Omits empty messages
+    """
+    for error in errors:
+        if isinstance(error, dbus.exceptions.DBusException):
+            error_str = error.get_dbus_message()
+        else:
+            error_str = str(error)
+        if error_str is not None and error_str != "":
+            yield error_str
+    return
+
+
+def handle_error(err):
+    """
+    Do the right thing with the given error, which may be the head of an error
+    chain.
+
+    :param Exception err: an exception
+    """
+
+    error_msgs = [msg for msg in get_error_msgs(get_errors(err))]
+    if error_msgs == []:
+        # It is unlikely that, within the whole chain of errors, there
+        # will be no message that is not an empty string. If there is
+        # there is some program error, so just raise the exception.
+        raise err
+
+    error_msg = ("%s    which in turn caused:%s" %
+                 (os.linesep, os.linesep)).join(reversed(error_msgs))
+
+    sys.exit("Execution failure caused by:%s%s" % (os.linesep, error_msg))
