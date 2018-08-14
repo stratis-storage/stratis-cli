a4107c05e48abf4fba8212f1cc4ba083b72daf66
mulhern <amulhern@redhat.com>
2018-08-14 10:39:49 -0400

    Be explicit about the type being compared with
    It is better to avoid boolean overloading where it is not needed.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/stratis_cli/_actions/_physical.py
+++ src/stratis_cli/_actions/_physical.py
@@ -73,7 +73,7 @@ class PhysicalActions():
 
         # If user specified a pool name we wil constrain output to that one
         # pool, else we will output all blockdevs.
-        if namespace.pool_name:
+        if namespace.pool_name is not None:
             (parent_pool_object_path, _) = unique(
                 pools(props={
                     'Name': namespace.pool_name
