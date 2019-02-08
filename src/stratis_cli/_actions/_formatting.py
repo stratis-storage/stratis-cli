a7d7d956f0248d01c0f649a78b7f8efdca060b84
mulhern <amulhern@redhat.com>
2019-02-08 10:13:13 -0500

    Abstract printing a row into a single method
    Printing the header row and printing data rows used to look sort of different
    but now the action is obviously identical, so might as well use the same
    function for both.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/stratis_cli/_actions/_formatting.py
+++ src/stratis_cli/_actions/_formatting.py
@@ -41,6 +41,29 @@ def _get_column_width_chars(column_width_cells, entry):
     return column_width_cells - (wcswidth(entry) - len(entry))
 
 
+def _print_row(file, num_columns, row, column_widths, column_alignments):
+    """
+    Print a single row in a table. The row might be the header row, or
+    a row of data items.
+
+    :param file: file to print to
+    :param int num_columns: the number of columns in the table
+    :param list row: the list of items to print
+    :param list column_widths: corresponding list of column widths
+    :param list column_alignments: correspoindg list of column alignment specs
+
+    Precondition: num_columns == len(row) == len(column_widths) == len(aligment)
+    Precondition: no elements of row have unprintable characters
+    """
+    for index in range(num_columns):
+        entry = row[index]
+        column_width_chars = _get_column_width_chars(column_widths[index],
+                                                     entry)
+        line = '{0:{align}{width}}'.format(
+            entry, align=column_alignments[index], width=column_width_chars)
+        print(line, end='', file=file)
+
+
 def print_table(column_headings, row_entries, alignment, file=sys.stdout):
     """
     Given the column headings and the row_entries, print a table.
@@ -71,22 +94,9 @@ def print_table(column_headings, row_entries, alignment, file=sys.stdout):
         for index in range(num_columns)
     ]
 
-    for index in range(num_columns):
-        entry = column_headings[index]
-        column_width_chars = _get_column_width_chars(column_lengths[index],
-                                                     entry)
-        line = '{0:{align}{width}}'.format(
-            entry, align=alignment[index], width=column_width_chars)
-        print(line, end='', file=file)
+    _print_row(file, num_columns, column_headings, column_lengths, alignment)
     print(file=file)
 
     for row in row_entries:
-        for index in range(num_columns):
-            entry = row[index]
-            column_width_chars = _get_column_width_chars(
-                column_lengths[index], entry)
-
-            line = '{0:{align}{width}}'.format(
-                entry, align=alignment[index], width=column_width_chars)
-            print(line, end='', file=file)
+        _print_row(file, num_columns, row, column_lengths, alignment)
         print(file=file)
