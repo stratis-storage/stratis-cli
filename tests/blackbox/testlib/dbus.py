145d1473ae813d678ceba348205c4178661ff059
Grace Chin <gchin@redhat.com>
2019-10-17 16:19:36 -0400

    git add

--- tests/blackbox/testlib/dbus.py
+++ tests/blackbox/testlib/dbus.py
@@ -145,28 +145,14 @@ class StratisDbus:
     def pool_create(pool_name, devices):
         """
         Create a pool
+        :return: result, return_code, return_string of CreatePool call
         """
-        pool_objects = {
-            path: obj_data[StratisDbus._POOL_IFACE]
-            for path, obj_data in StratisDbus._get_managed_objects().items()
-            if StratisDbus._POOL_IFACE in obj_data
-            and obj_data[StratisDbus._POOL_IFACE]["Name"].startswith(TEST_PREF)
-        }
-
-        pool_object_paths = [
-            path
-            for path, pool_obj in pool_objects.items()
-            if pool_obj["Name"] == pool_name
-        ]
-        if pool_object_paths == []:
-            return None
-
         iface = dbus.Interface(
             StratisDbus._BUS.get_object(StratisDbus._BUS_NAME, StratisDbus._TOP_OBJECT),
             StratisDbus._MNGR_IFACE,
         )
         return iface.CreatePool(
-            pool_name, [0, 0], devices, timeout=StratisDbus._TIMEOUT
+            pool_name, (dbus.Boolean(False), dbus.UInt16(0)), devices, timeout=StratisDbus._TIMEOUT
         )
 
     @staticmethod
@@ -226,7 +212,7 @@ class StratisDbus:
     @staticmethod
     def pool_add_cache(pool_name, devices):
         """
-        Adding cache to a pool
+        Add a block device as a cache device
         """
         objects = StratisDbus._get_managed_objects().items()
 
@@ -254,7 +240,7 @@ class StratisDbus:
     @staticmethod
     def pool_add_data(pool_name, devices):
         """
-        Adding data to a pool
+        Add a disk to an existing pool
         """
         objects = StratisDbus._get_managed_objects().items()
 
@@ -280,44 +266,16 @@ class StratisDbus:
         return iface.AddDataDevs(devices, timeout=StratisDbus._TIMEOUT)
 
     @staticmethod
-    def filesystem_create(pool_name, fs_name):
+    def filesystem_create(pool_object_path, fs_name):
         """
         Create a filesystem
         """
-        objects = StratisDbus._get_managed_objects().items()
-
-        pool_objects = {
-            path: obj_data[StratisDbus._POOL_IFACE]
-            for path, obj_data in objects
-            if StratisDbus._POOL_IFACE in obj_data
-            and obj_data[StratisDbus._POOL_IFACE]["Name"].startswith(TEST_PREF)
-        }
-        fs_objects = {
-            path: obj_data[StratisDbus._FS_IFACE]
-            for path, obj_data in objects
-            if StratisDbus._FS_IFACE in obj_data
-            and obj_data[StratisDbus._FS_IFACE]["Name"].startswith(TEST_PREF)
-        }
-
-        pool_object_paths = [
-            path
-            for path, pool_obj in pool_objects.items()
-            if pool_obj["Name"] == pool_name
-        ]
-        if pool_object_paths == []:
-            return None
-
-        fs_object_paths = [
-            path for path, fs_obj in fs_objects.items() if fs_obj["Name"] == fs_name
-        ]
-        if fs_object_paths == []:
-            return None
-
         iface = dbus.Interface(
-            StratisDbus._BUS.get_object(StratisDbus._BUS_NAME, pool_object_paths[0]),
+            StratisDbus._BUS.get_object(StratisDbus._BUS_NAME, StratisDbus._TOP_OBJECT),
             StratisDbus._POOL_IFACE,
         )
-        return iface.CreateFilesystems(fs_object_paths, timeout=StratisDbus._TIMEOUT)
+
+        return iface.CreateFilesystems([pool_object_path, fs_name], timeout=StratisDbus._TIMEOUT)
 
     @staticmethod
     def fs_destroy(pool_name, fs_name):
