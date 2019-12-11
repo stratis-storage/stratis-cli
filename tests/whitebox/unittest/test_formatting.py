1966c66926e71d052f4c8959158bb513d845f4f6
Grace Chin <gchin@redhat.com>
2019-12-11 14:01:52 -0500

    Add another test case

--- tests/whitebox/unittest/test_formatting.py
+++ tests/whitebox/unittest/test_formatting.py
@@ -41,8 +41,15 @@ class FormattingTestCase(unittest.TestCase):
                 "Oct 05 2018 16:24",
                 "/dev/stratis/yes_you_can/☺",
             ],
+            [
+                "yes_you_can",
+                "漢字",
+                "546 MiB",
+                "Oct 10 2018 09:37",
+                "/dev/stratis/yes_you_can/漢字",
+            ],
         ]
-        print_table(table[0], table[1:], [">", ">", "<", "<", "<"], self.output)
+        print_table(table[0], table[1:], ["<", "<", "<", "<", "<"], self.output)
 
     def testContainsNewLine(self):
         """
@@ -55,4 +62,64 @@ class FormattingTestCase(unittest.TestCase):
         Test that the table contains the correct number of lines
         """
         self.output.seek(0)
-        self.assertEqual(len(self.output.readlines()), 2)
+        self.assertEqual(len(self.output.readlines()), 3)
+
+
+class FormattingTestCase2(unittest.TestCase):
+    """
+    Test formatting.
+    """
+
+    def setUp(self):
+        self.output = io.StringIO()
+
+        # pylint: disable=bad-continuation
+        table = [
+            [u"Pool Na\u030ame", u"Na\u030ame", "Used", "Created", "Device", "UUID"],
+            [
+                "unicode",
+                "e",
+                "546 MiB",
+                "Feb 07 2019 15:33",
+                "/stratis/unicode/e",
+                "3bf22806a6df4660aa527d646209595f",
+            ],
+            [
+                "unicode",
+                "eeee",
+                "546 MiB",
+                "Feb 07 2019 15:33",
+                "/stratis/unicode/eeee",
+                "17101e39e72e423c90d8be5cb37c055b",
+            ],
+            [
+                "unicodé",
+                "é",
+                "546 MiB",
+                "Feb 07 2019 15:33",
+                "/stratis/unicodé/é",
+                "0c2caf641dde41beb40bed6911f75c74",
+            ],
+            [
+                "unicodé",
+                "éééé",
+                "546 MiB",
+                "Feb 07 2019 15:33",
+                "/stratis/unicodé/éééé",
+                "4ecacb15fb64453191d7da731c5f1601",
+            ],
+        ]
+        print_table(table[0], table[1:], ["<", "<", "<", "<", "<", "<"], self.output)
+
+    def testContainsNewLine(self):
+        """
+        Test that the table contains a new line
+        """
+        self.assertRegex(self.output.getvalue(), "\n")
+
+    def testContainsCorrectNumberOfLines(self):
+        """
+        Test that the table contains the correct number of lines
+        """
+        self.output.seek(0)
+        self.assertEqual(len(self.output.readlines()), 5)
