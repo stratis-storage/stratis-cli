a765ae285acb642841d8f7db6cba7bfdddc39384
Leah Leshchinsky <lleshchi@redhat.com>
2020-02-20 11:10:53 -0500

    Revision - add description
    Signed-off-by: Leah Leshchinsky <lleshchi@redhat.com>

--- tests/whitebox/monkey_patching/test_timeout.py
+++ tests/whitebox/monkey_patching/test_timeout.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Test D-Bus Timeout Environment Variable
+Test proper handling of D-Bus NoReply exception
 """
 # isort: STDLIB
 from os import environ
