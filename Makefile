bc05a6f8ccbd1e2df5f5a445550f7a0268ed5dad
Leah Leshchinsky <lleshchi@redhat.com>
2020-05-07 09:25:34 -0400

    Check for stratisd version compatibility
    Query the version of stratisd before completing each action
    and confirm that it is compatible with the current version
    requirements of the CLI.
    
    Add monkey patch test to check handling of incompatible
    stratid version. This allows for full coverage.
    
    Signed-off-by: Leah Leshchinsky <lleshchi@redhat.com>

--- Makefile
+++ Makefile
@@ -57,6 +57,7 @@ coverage-no-html:
 	python3 -m coverage run --timid --branch -m pytest ./tests/whitebox/integration
 	python3 -m coverage run --timid --branch -a -m pytest ./tests/whitebox/unittest
 	python3 -m coverage run --timid --branch -a -m pytest ./tests/whitebox/monkey_patching/test_keyboard_interrupt.py
+	python3 -m coverage run --timid --branch -a -m pytest ./tests/whitebox/monkey_patching/test_stratisd_version.py
 	python3 -m coverage report -m --fail-under=100 --show-missing --include="./src/*"
 
 .PHONY: coverage
@@ -65,6 +66,9 @@ coverage: coverage-no-html
 
 keyboard-interrupt-test:
 	py.test-3 ${PYTEST_OPTS} ./tests/whitebox/monkey_patching/test_keyboard_interrupt.py
+
+stratisd-version-test:
+	py.test-3 ${PYTEST_OPTS} ./tests/whitebox/monkey_patching/test_stratisd_version.py
 
 test-travis:
 	py.test ${PYTEST_OPS} ./tests/whitebox/unittest
---
+++ tests/whitebox/monkey_patching/test_stratisd_version.py
@@ -0,0 +1,52 @@
+# Copyright 2020 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Test handling of incompatible stratisd version
+"""
+
+# isort: LOCAL
+import stratis_cli
+from stratis_cli import StratisCliErrorCodes
+from stratis_cli._errors import StratisCliStratisdVersionError
+
+from .._misc import SimTestCase
+
+_ERROR = StratisCliErrorCodes.ERROR
+
+
+class StratisdVersionTestCase(SimTestCase):
+    """
+    Test behavior of stratis on incompatible versions of stratisd.
+    """
+
+    def test_outdated_stratisd_version(self):
+        """
+        Verify that an outdated version of stratisd will produce a StratisCliStratisdVersionError.
+        """
+
+        def outdated_stratisd_version(_):
+            """
+            Return an outdated stratisd version.
+            """
+            return "1.0.0"
+
+        from stratis_cli._actions import _data
+
+        # pylint: disable=protected-access
+        stratis_cli._actions._data.Manager.Properties.Version.Get = (
+            outdated_stratisd_version
+        )
+
+        command_line = ["--propagate", "daemon", "version"]
+        self.check_error(StratisCliStratisdVersionError, command_line, _ERROR)
