606e8bbc37f7084fb887cedf2dc26f0238a4b71f
mulhern <amulhern@redhat.com>
2020-06-03 11:46:48 -0400

    Expect non-root user to be able to use Report interface
    Agree with the stratisd.conf file that any user can use the Report
    interface.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/blackbox/stratisd_cert.py
+++ tests/blackbox/stratisd_cert.py
@@ -568,7 +568,7 @@ class StratisCertify(unittest.TestCase):  # pylint: disable=too-many-public-meth
         """
         Test that getting a valid report fails when root permissions are dropped.
         """
-        self._test_permissions(StratisDbus.get_report, ["errored_pool_report"], True)
+        self._test_permissions(StratisDbus.get_report, ["errored_pool_report"], False)
 
 
 def main():
