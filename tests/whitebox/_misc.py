5b76ec0548ecf3c12807f18cf69e77075d323a09
Bryan Gurney <bgurney@redhat.com>
2021-02-23 08:35:03 -0500

    move pylint disable up one level
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- tests/whitebox/_misc.py
+++ tests/whitebox/_misc.py
@@ -67,8 +67,8 @@ class _Service:
             raise RuntimeError(
                 "STRATISD environment variable must be set to absolute path of stratisd executable"
             )
-        self._stratisd = (
-            subprocess.Popen(  # pylint: disable=attribute-defined-outside-init
+        self._stratisd = (  # pylint: disable=attribute-defined-outside-init
+            subprocess.Popen(
                 [os.path.join(stratisd_var), "--sim"],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
