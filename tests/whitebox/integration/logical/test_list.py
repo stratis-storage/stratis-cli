a03ef0392598253ddb023d1a932d51f4a85d8b49
mulhern <amulhern@redhat.com>
2021-06-03 15:40:24 -0400

    Add facility for making randomly generated device names distinct
    Use in the place where failing to ensure unique names caused a test bug
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/whitebox/integration/logical/test_list.py
+++ tests/whitebox/integration/logical/test_list.py
@@ -21,7 +21,13 @@ from dbus_client_gen import DbusClientUniqueResultError
 # isort: LOCAL
 from stratis_cli import StratisCliErrorCodes
 
-from .._misc import RUNNER, TEST_RUNNER, SimTestCase, device_name_list
+from .._misc import (
+    RUNNER,
+    TEST_RUNNER,
+    SimTestCase,
+    device_name_list,
+    split_device_list,
+)
 
 _DEVICE_STRATEGY = device_name_list(1)
 
@@ -126,18 +132,23 @@ class List5TestCase(SimTestCase):
         Start the stratisd daemon with the simulator.
         """
         super().setUp()
-        command_line = ["pool", "create", self._POOLNAMES[0]] + _DEVICE_STRATEGY()
+
+        num_pools = 3
+        devices = device_name_list(num_pools, 10 * num_pools, True)()
+        device_lists = split_device_list(devices, num_pools)
+
+        command_line = ["pool", "create", self._POOLNAMES[0]] + device_lists[0]
         RUNNER(command_line)
 
         command_line = ["filesystem", "create", self._POOLNAMES[0], self._VOLUMES[0]]
         RUNNER(command_line)
         command_line = ["filesystem", "create", self._POOLNAMES[0], self._VOLUMES[1]]
         RUNNER(command_line)
-        command_line = ["pool", "create", self._POOLNAMES[1]] + _DEVICE_STRATEGY()
+        command_line = ["pool", "create", self._POOLNAMES[1]] + device_lists[1]
         RUNNER(command_line)
         command_line = ["filesystem", "create", self._POOLNAMES[1], self._VOLUMES[2]]
         RUNNER(command_line)
-        command_line = ["pool", "create", self._POOLNAMES[2]] + _DEVICE_STRATEGY()
+        command_line = ["pool", "create", self._POOLNAMES[2]] + device_lists[2]
         RUNNER(command_line)
 
     def test_list_one(self):
