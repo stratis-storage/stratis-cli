7c31005a2e8fc14a6b40f2c5506bc4097a22e9d3
Bryan Gurney <bgurney@redhat.com>
2023-08-08 15:01:14 -0400

    Ignore bandit B603 errors for processed input

--- src/stratis_cli/_actions/_report_pool.py
+++ src/stratis_cli/_actions/_report_pool.py
@@ -111,9 +111,10 @@ class Default(Report):  # pylint: disable=too-few-public-methods
             )
         )
 
-        # replace the lines below with the correct call to lsblk
         blkdevs = [dev.Devnode() for dev in modevs]
-        subprocess.run(["/usr/bin/lsblk", "-i"] + blkdevs, check=True)
+        # Ignore bandit B603 errors.  Input comes from D-Bus and has
+        # been processed.
+        subprocess.run(["/usr/bin/lsblk", "-i"] + blkdevs, check=True)  # nosec B603
 
 
 class Stopped(Report):  # pylint: disable=too-few-public-methods
@@ -154,7 +155,8 @@ class Stopped(Report):  # pylint: disable=too-few-public-methods
         if stopped_pool is None:
             raise StratisCliResourceNotFoundError("list", selection_value)
 
-        # Substitute lsblk call here
+        # Ignore bandit B603 errors.  Input comes from D-Bus and has
+        # been processed.
         for dev in stopped_pool["devs"]:
             print(dev)
-            subprocess.run(["/usr/bin/lsblk", "-i", str(dev)], check=True)
+            subprocess.run(["/usr/bin/lsblk", "-i", str(dev)], check=True)  # nosec B603
