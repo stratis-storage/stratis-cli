c4d4c502342ec8563283f7a80477f5e3a2890733
mulhern <amulhern@redhat.com>
2023-11-27 18:52:56 -0500

    Add method modifications
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/stratis_cli/_actions/_dynamic.py
+++ src/stratis_cli/_actions/_dynamic.py
@@ -57,6 +57,31 @@ class ClassKey(Enum):
     REPORT = ClassInfo("Report", REPORT_INTERFACE)
 
 
+def _add_abs_path_assertion(klass, method_name, key):
+    """
+    Set method_name of method_klass to a new method which checks that the
+    device paths values at key are absolute paths.
+
+    :param klass: the klass to which this metthod belongs
+    :param str method_name: the name of the method
+    :param str key: the key at which the paths can be found in the arguments
+    """
+    method_class = getattr(klass, "Methods")
+    orig_method = getattr(method_class, method_name)
+
+    def new_method(proxy, args):
+        """
+        New path method
+        """
+        rel_paths = [path for path in args[key] if not os.path.isabs(path)]
+        assert (
+            rel_paths == []
+        ), f"Precondition violated: paths {', '.join(rel_paths)} should be absolute"
+        return orig_method(proxy, args)
+
+    setattr(method_class, method_name, new_method)
+
+
 def make_dyn_class(key):
     """
     Dynamically generate a class from introspection specification.
@@ -64,11 +89,26 @@ def make_dyn_class(key):
     :param ClassKey key: key that identifies the class to make
     """
     try:
-        return make_class(
+        klass = make_class(
             key.value.name,
             ET.fromstring(SPECS[key.value.interface_name]),  # nosec B314
             TIMEOUT,
         )
+
+        try:
+            if key == ClassKey.MANAGER:
+                _add_abs_path_assertion(klass, "CreatePool", "devices")
+        except AttributeError as err:  # pragma: no cover
+            # This can only happen if the expected method is missing from
+            # the XML spec or code generation has a bug, we will never
+            # test for these conditions.
+            raise StratisCliGenerationError(
+                "Malformed class definition; could not access a class or "
+                "method in the generated class definition"
+            ) from err
+
+        return klass
+
     except DPClientGenerationError as err:  # pragma: no cover
         raise StratisCliGenerationError(
             f"Failed to generate class {key.value.name} needed for invoking "
