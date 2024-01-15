73f5ec1ef567ad01003ea519faeefc0f4751d751
mulhern <amulhern@redhat.com>
2024-01-14 22:20:50 -0500

    Add filesystem selection in parser
    Ignore it in listing action, temporarily.
    
    Signed-off-by: mulhern <amulhern@redhat.com>
    (cherry picked from commit 04616d4d3b6b7fb972c1b232a15ff49450d50701)

--- src/stratis_cli/_actions/_list_filesystem.py
+++ src/stratis_cli/_actions/_list_filesystem.py
@@ -24,11 +24,60 @@ from ._constants import TOP_OBJECT
 from ._formatting import TOTAL_USED_FREE, get_property, print_table, size_triple
 
 
-def list_filesystems(uuid_formatter, *, selection=None):
+def list_filesystems(
+    uuid_formatter, *, pool_name=None, fs_id=None
+):  # pylint: disable=too-many-locals
     """
     List the specified information about filesystems.
     """
-    klass = List(uuid_formatter, selection=selection)
+    assert fs_id is None or pool_name is not None
+
+    # pylint: disable=import-outside-toplevel
+    from ._data import MOFilesystem, MOPool, ObjectManager, filesystems, pools
+
+    proxy = get_object(TOP_OBJECT)
+    managed_objects = ObjectManager.Methods.GetManagedObjects(proxy, {})
+
+    if pool_name is None:
+        props = None
+        pool_object_path = None
+        fs_props = None
+        requires_unique = False
+    else:
+        props = {"Name": pool_name}
+        pool_object_path = next(
+            pools(props=props).require_unique_match(True).search(managed_objects)
+        )[0]
+        fs_props = {"Pool": pool_object_path} | (
+            {} if fs_id is None else fs_id.managed_objects_key()
+        )
+        requires_unique = fs_id is not None
+
+    path_to_name = dict(
+        (path, MOPool(info).Name())
+        for path, info in pools(props=props).search(managed_objects)
+    )
+
+    filesystems_with_props = [
+        MOFilesystem(info)
+        for objpath, info in filesystems(props=fs_props)
+        .require_unique_match(requires_unique)
+        .search(managed_objects)
+    ]
+
+    if fs_id is None:
+        klass = List(
+            uuid_formatter,
+            filesystems_with_props,
+            path_to_name,
+        )
+    else:
+        klass = List(
+            uuid_formatter,
+            filesystems_with_props,
+            path_to_name,
+        )
+
     klass.list_filesystems()
 
 
@@ -37,17 +86,16 @@ class List:  # pylint: disable=too-few-public-methods
     Handle listing a filesystem or filesystems.
     """
 
-    def __init__(self, uuid_formatter, *, selection=None):
+    def __init__(self, uuid_formatter, filesystems_with_props, path_to_name):
         """
         Initialize a List object.
         :param uuid_formatter: function to format a UUID str or UUID
         :param uuid_formatter: str or UUID -> str
         :param bool stopped: whether to list stopped pools
-        :param selection: how to select the pool to display
-        :type selection: pair of str * object or NoneType
         """
         self.uuid_formatter = uuid_formatter
-        self.selection = selection
+        self.filesystems_with_props = filesystems_with_props
+        self.path_to_name = path_to_name
 
     def list_filesystems(self):
         """
@@ -69,37 +117,9 @@ class List:  # pylint: disable=too-few-public-methods
             limit = get_property(dbus_props.SizeLimit(), Range, None)
             return f'{size_triple(total, used)} / {"None" if limit is None else limit}'
 
-        # pylint: disable=import-outside-toplevel
-        from ._data import MOFilesystem, MOPool, ObjectManager, filesystems, pools
-
-        proxy = get_object(TOP_OBJECT)
-        managed_objects = ObjectManager.Methods.GetManagedObjects(proxy, {})
-
-        props = None if self.selection is None else self.selection.managed_objects_key()
-
-        filesystems_with_props = [
-            MOFilesystem(info)
-            for objpath, info in filesystems(
-                props=None
-                if props is None
-                else {
-                    "Pool": next(
-                        pools(props=props)
-                        .require_unique_match(True)
-                        .search(managed_objects)
-                    )[0]
-                }
-            ).search(managed_objects)
-        ]
-
-        path_to_name = dict(
-            (path, MOPool(info).Name())
-            for path, info in pools(props=props).search(managed_objects)
-        )
-
         tables = [
             (
-                path_to_name[mofilesystem.Pool()],
+                self.path_to_name[mofilesystem.Pool()],
                 mofilesystem.Name(),
                 filesystem_size_quartet(mofilesystem),
                 date_parser.parse(mofilesystem.Created())
@@ -108,7 +128,7 @@ class List:  # pylint: disable=too-few-public-methods
                 mofilesystem.Devnode(),
                 self.uuid_formatter(mofilesystem.Uuid()),
             )
-            for mofilesystem in filesystems_with_props
+            for mofilesystem in self.filesystems_with_props
         ]
 
         print_table(
