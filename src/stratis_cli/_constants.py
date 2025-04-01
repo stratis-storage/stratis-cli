7ff493dc74f56fe21cdd4d11e0a592a49a068423
mulhern <amulhern@redhat.com>
2025-04-01 13:34:10 -0400

    Add __str__ method to IdType
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/stratis_cli/_constants.py
+++ src/stratis_cli/_constants.py
@@ -38,8 +38,11 @@ class IdType(Enum):
     Whether the pool identifier is a UUID or a name.
     """
 
-    UUID = 0
-    NAME = 1
+    UUID = "UUID"
+    NAME = "name"
+
+    def __str__(self):
+        return self.value
 
 
 class Id:
