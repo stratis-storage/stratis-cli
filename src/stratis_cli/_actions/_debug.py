44bc2512b92d1e3f346d10342801e490143e57f4
mulhern <amulhern@redhat.com>
2025-04-01 21:36:00 -0400

    Fix up debug commands to use Id construction
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/stratis_cli/_actions/_debug.py
+++ src/stratis_cli/_actions/_debug.py
@@ -19,6 +19,7 @@ Miscellaneous debugging actions.
 import json
 import os
 
+from .._constants import FilesystemId, IdType, PoolId
 from .._errors import StratisCliEngineError, StratisCliSynthUeventError
 from .._stratisd_constants import StratisdErrors
 from ._connection import get_object
@@ -82,13 +83,15 @@ class PoolDebugActions:  # pylint: disable=too-few-public-methods
 
         proxy = get_object(TOP_OBJECT)
         managed_objects = ObjectManager.Methods.GetManagedObjects(proxy, {})
-        props = (
-            {"Uuid": namespace.uuid.hex}
+        pool_id = (
+            PoolId(IdType.UUID, namespace.uuid)
             if namespace.name is None
-            else {"Name": namespace.name}
+            else PoolId(IdType.NAME, namespace.name)
         )
         (pool_object_path, _) = next(
-            pools(props=props).require_unique_match(True).search(managed_objects)
+            pools(props=pool_id.managed_objects_key())
+            .require_unique_match(True)
+            .search(managed_objects)
         )
         print(pool_object_path)
 
@@ -103,13 +106,15 @@ class PoolDebugActions:  # pylint: disable=too-few-public-methods
         proxy = get_object(TOP_OBJECT)
         managed_objects = ObjectManager.Methods.GetManagedObjects(proxy, {})
 
-        props = (
-            {"Uuid": namespace.uuid.hex}
+        pool_id = (
+            PoolId(IdType.UUID, namespace.uuid)
             if namespace.name is None
-            else {"Name": namespace.name}
+            else PoolId(IdType.NAME, namespace.name)
         )
         (pool_object_path, _) = next(
-            pools(props=props).require_unique_match(True).search(managed_objects)
+            pools(props=pool_id.managed_objects_key())
+            .require_unique_match(True)
+            .search(managed_objects)
         )
 
         (metadata, return_code, message) = Pool.Methods.Metadata(
@@ -143,13 +148,15 @@ class FilesystemDebugActions:  # pylint: disable=too-few-public-methods
 
         proxy = get_object(TOP_OBJECT)
         managed_objects = ObjectManager.Methods.GetManagedObjects(proxy, {})
-        props = (
-            {"Uuid": namespace.uuid.hex}
+        fs_id = (
+            FilesystemId(IdType.UUID, namespace.uuid)
             if namespace.name is None
-            else {"Name": namespace.name}
+            else FilesystemId(IdType.NAME, namespace.name)
         )
         (fs_object_path, _) = next(
-            filesystems(props=props).require_unique_match(True).search(managed_objects)
+            filesystems(props=fs_id.managed_objects_key())
+            .require_unique_match(True)
+            .search(managed_objects)
         )
         print(fs_object_path)
 
